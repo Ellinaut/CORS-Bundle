f1c141aafebd335e5205eebc331e499dc12e5123
pmarien <philipp.marien@gmail.com>
2022-08-02 16:19:38 +0200

    fixed config

--- src/Listener/RequestListener.php
+++ src/Listener/RequestListener.php
@@ -2,23 +2,42 @@
 
 namespace Ellinaut\CorsBundle\Listener;
 
+use Ellinaut\CorsBundle\Provider\ConfigurationProvider;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
 
 /**
  * @author Philipp Marien
  */
 class RequestListener extends AbstractCorsListener
 {
+    public function __construct(
+        ConfigurationProvider $configurationProvider,
+        private readonly RequestMatcherInterface $requestMatcher
+    ) {
+        parent::__construct($configurationProvider);
+    }
+
     public function onKernelRequest(RequestEvent $event): void
     {
         if (!$this->configurationProvider->hasConfig($event->getRequest()->getPathInfo())) {
             return;
         }
 
-        $config = $this->configurationProvider->getConfig($event->getRequest()->getPathInfo());
-        if ($config->get('handle_options')) {
-            $event->setResponse(new Response());
+        if ($event->getRequest()->getMethod() !== Request::METHOD_OPTIONS) {
+            return;
+        }
+
+        try {
+            $this->requestMatcher->matchRequest($event->getRequest());
+        } catch (\Throwable $throwable) {
+            // if no options request is configured explicitly...
+            $config = $this->configurationProvider->getConfig($event->getRequest()->getPathInfo());
+            if ($config->get('handle_options')) {
+                $event->setResponse(new Response());
+            }
         }
     }
 }
